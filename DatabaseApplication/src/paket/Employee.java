6be13fa9c3de553cbc405ed4756a6a19721bcdf6
Yasin Akdoğan <yasin_akdogan15@hotmail.com>
2019-12-28 17:41:38 +0300

    Finish.

---
+++ DatabaseApplication/src/paket/Employee.java
@@ -0,0 +1,41 @@
+package paket;
+
+public class Employee {
+	
+	private String name,surname,email;
+
+	public Employee(String name, String surname, String email) {
+		super();
+		this.name = name;
+		this.surname = surname;
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	
+}
